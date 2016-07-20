8d3c01c81e1bead868e9daeaa581237961748695
Matt <mnuecht@gmail.com>
2016-07-20 09:43:23 -0700

    Vector update

--- vector.cpp
+++ vector.cpp
@@ -1,6 +1,25 @@
 #include "includes.h"
 #include <vector>
 
+vector<int> vec(4,100);
+
+void printVec() {
+	printf("Vector size: %li\n", vec.size());
+	printf("Vector capacity: %lu\n", vec.capacity());
+	for (int i = 0; i < vec.size(); i++) {
+		printf("vec[%i] = %i\n", i, vec[i]);
+	}
+	printf("\n");
+}
+
 void testVector() {
 	printf("testVector\n");
+	printVec();
+	
+	vec.push_back(33);
+	printVec();
+	
+	vec.reserve(12);
+	vec.resize(7);
+	printVec();
 }
